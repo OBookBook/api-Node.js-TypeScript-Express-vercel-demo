db74ea750e52b179f94401a45840bebc0c89ecc0
N.I <iworksWeb@gmail.com>
2025-06-13 11:39:56 +0900

    add cors to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,18 @@ import { hello } from "./api/hello";
 const app = express();
 const PORT = 3010;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
