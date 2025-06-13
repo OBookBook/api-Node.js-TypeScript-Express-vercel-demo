065ce30773397f94619d66fc9290aae0a417b680
N.I <iworksWeb@gmail.com>
2025-06-13 10:57:28 +0900

    use hello api to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import { hello } from "./api/hello";
 
 const app = express();
 const PORT = 3010;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// APIルート
+app.get("/api/hello", hello);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
